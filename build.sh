5a3c0c8a384536694cda8eebeb88052d3f280cb0
Luis Pedro Morales Rodriguez <luismorarod98@gmail.com>
2020-12-21 21:31:26 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
-echo "1/3 Installing dependencies"
+echo "1/4 Installing dependencies"
 npm install
 npm install bootstrap --save
 npm install react-router-dom --save
 
-echo "2/3 Starting build of react applicaion..."
+echo "2/4 Starting build of react applicaion..."
 npm run build
 
-echo "3/3 Packing application into docker image..."
-docker build . -t users-crud-frontend
+echo "3/4 Packing application into docker image..."
+docker build . -t luispedromorales/users-crud-frontend:latest
 
-echo "3/3 Steps completed"
+echo "4/4 Pushing to Dockerhub"
+docker login -u luispedromorales -p $DOCKERHUB_PASSWORD
+docker push luispedromorales/users-crud-frontend:latest
+
+echo "4/4 Steps completed"
