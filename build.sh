f31227bcbe9e11917974958e9d50521c32e0cfe5
Luis Pedro Morales Rodriguez <luismorarod98@gmail.com>
2020-12-21 20:49:22 -0600

    add dependencies installation to ./build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+echo "1/3 Installing dependencies"
+npm install
+npm install bootstrap --save
+npm install react-router-dom --save
+
 echo "1/2 Starting build of react applicaion..."
 npm run build
 
